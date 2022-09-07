90b0005bacf6bf44eb6856e9bb68aacc1db8b9e9
Leonardo Ferreira <ferreira.sleonardo@gmail.com>
2022-09-07 17:10:43 -0300

    first commit

---
+++ Model/Usuario.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenLims.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public decimal Salary { get; set; }
+        public string Currency { get; set; }
+        public JsonContent Groups { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+
+
+    }
+}
